0006ed22c6cebb15243aa2e730998e1fd0a89e70
Lup Yuen Lee <luppy@appkaki.com>
2026-03-29 15:40:03 +0800

    JSON Stream OK
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use build_html::{Html, Table};
 use struson::{
-    reader::{
-        simple::{SimpleJsonReader, ValueReader},
-    },
+    json_path,
+    reader::{JsonReader, JsonStreamReader, simple::{SimpleJsonReader, ValueReader}}, writer::{JsonStreamWriter, JsonWriter}
 };
 
 fn main() {
@@ -17,18 +16,19 @@ fn main() {
         .to_html_string();
     println!("{}", html_table);
 
-    // Given a Run ID (Job ID): Locate the PR
+    // Given a Run ID (Job ID): Locate the Job + PR
     let run_id = 1234;
     let json = 
         r#"
         [
-            {"a1": true, "a2": false},
-            {"b1": true, "b2": false}
+            {"a": true, "b": true},
+            {"a": false, "b": false}
         ]
         "#;
     let json_reader = SimpleJsonReader::new(json.as_bytes());
     let return_value = json_reader.read_array_items(|array_reader| {
-        // If the Run ID matches, remember the PR
+        // If the Run ID matches, remember the Job + PR
+        // println!("array_reader: {:?}", array_reader);
         array_reader.read_object_owned_names(|name, value_reader| {            
             let val = value_reader.read_bool().unwrap();
             println!("{}: {}", name, val);
@@ -38,4 +38,23 @@ fn main() {
         Ok(())
     }).unwrap();
     println!("return_value: {:?}", return_value);
+
+    // Jump to the Found Index and read the Job + PR
+    let index = 1;
+    let mut json_reader = JsonStreamReader::new(json.as_bytes());
+    let path = &json_path![index];
+    // println!("json_reader before: {:?}\n", json_reader);
+    json_reader.seek_to(path).unwrap();
+    // println!("json_reader after: {:?}\n", json_reader);
+
+    // Extract the Job + PR
+    let mut writer = Vec::<u8>::new();
+    let mut json_writer = JsonStreamWriter::new(&mut writer);
+    // json_writer.begin_object().unwrap();
+    // json_writer.name("embedded").unwrap();
+    json_reader.transfer_to(&mut json_writer).unwrap();
+    // json_writer.end_object().unwrap();
+    json_writer.finish_document().unwrap() ;
+    let job_pr = String::from_utf8(writer).unwrap();
+    println!("job_pr: {}", job_pr);
 }
