cacdcf3c39620ba867913562aa005f76fa0c3db2
Lup Yuen Lee <luppy@appkaki.com>
2026-03-29 14:42:00 +0800

    JSON Stream OK
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::thread::sleep;
+
 use build_html::{Html, Table};
+use struson::{
+    json_path,
+    reader::{
+        JsonStreamReader, JsonSyntaxError, ReaderError, SyntaxErrorKind, ValueType,
+        simple::{SimpleJsonReader, ValueReader},
+    },
+};
 
 fn main() {
+    println!("export-nuttx-builds");
     let source_table = [
         [1, 2, 3],
         [4, 5, 6],
@@ -10,4 +20,22 @@ fn main() {
         .with_header_row(['A', 'B', 'C'])
         .to_html_string();
     println!("{}", html_table);
+
+    let json = 
+        r#"
+        [
+            {"nested1": true},
+            {"nested2": true}
+        ]
+        "#;
+    let json_reader = SimpleJsonReader::new(json.as_bytes());
+    let return_value = json_reader.read_array_items(|mut array_reader| {
+        array_reader.read_object_owned_names(|name, value_reader| {            
+            let val = value_reader.read_bool().unwrap();
+            println!("{}: {}", name, val);
+            Ok(())
+        })?;
+        Ok(())
+    }).unwrap();
+    println!("return_value: {:?}", return_value);
 }
