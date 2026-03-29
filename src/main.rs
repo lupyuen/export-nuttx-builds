75ae2582bb935425d700f1424a0b2b1d56037d63
Lup Yuen Lee <luppy@appkaki.com>
2026-03-29 18:26:18 +0800

    Read JSON File OK
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,16 @@ fn main() {
         .to_html_string();
     println!("{}", html_table);
 
-    // Given a Run ID (Job ID): Locate the Job-PR
-    let run_id = 23688473202;
-    let json = 
-        r#"
-        [
-            {"job_databaseId": 23688473202, "a": true, "b": true},
-            {"job_databaseId": 23688473199, "a": false, "b": false}
-        ]
-        "#;
-    let json_reader = SimpleJsonReader::new(json.as_bytes());
+    // Given a Run ID (Job ID): Locate the Job-PR from the JSON File
+    // let run_id = 23653869993;  // sim-02:sim:login: >>>> WARNING: YOU ARE USING DEFAULT PASSWORD KEYS (CONFIG_FSUTILS_PASSWD_KEY1-4)!!! PLEASE CHANGE IT!!! <<<< \n 17d16 \n < CONFIG_BOARD_ETC_ROMFS_PASSWD_PASSWORD=\"Administrator\" \n Saving the new configuration file
+    // let run_id = 23669957941;  // Successful
+    let run_id = 23679432579;  // Test Retry
+    // let run_id = 1234;  // Doesn't exist
+
+    // Open the JSON File and create a Streaming JSON Reader
+    let file = std::fs::read(JOB_PR_JSON).unwrap();
+    let json_reader = SimpleJsonReader::new(file.as_slice());
+
     // For each Job-PR record in the array...
     let mut index = Option::<usize>::None;
     let mut i = 0;
@@ -38,7 +38,6 @@ fn main() {
             // If the Run ID matches, remember the Found Index
             if name == "job_databaseId" {
                 let val: u64 = value_reader.read_number().unwrap().unwrap();
-                println!("{}: {}", name, val);
                 if val == run_id {
                     index = Some(i);
                     println!("Found Index: {}", index.unwrap());
@@ -46,26 +45,27 @@ fn main() {
             }
             Ok(())
         })?;
-        println!("Item Done");
         i += 1;
         Ok(())
     }).unwrap();
 
-    // Jump to the Found Index
+    // Quit if index not found
+    if index.is_none() {
+        println!("Run ID {run_id} not found in {JOB_PR_JSON}");
+        return;
+    }
     let index = index.unwrap() as u32;
-    let mut json_reader = JsonStreamReader::new(json.as_bytes());
+
+    // Jump to the Found Index in the Job-PR array
+    let file = std::fs::read(JOB_PR_JSON).unwrap();
+    let mut json_reader = JsonStreamReader::new(file.as_slice());
     let path = &json_path![index];
-    // println!("json_reader before: {:?}\n", json_reader);
     json_reader.seek_to(path).unwrap();
-    // println!("json_reader after: {:?}\n", json_reader);
 
     // Extract the Job-PR
     let mut writer = Vec::<u8>::new();
     let mut json_writer = JsonStreamWriter::new(&mut writer);
-    // json_writer.begin_object().unwrap();
-    // json_writer.name("embedded").unwrap();
     json_reader.transfer_to(&mut json_writer).unwrap();
-    // json_writer.end_object().unwrap();
     json_writer.finish_document().unwrap() ;
     let job_pr = String::from_utf8(writer).unwrap();
     println!("job_pr: {}", job_pr);
