f6c797629d03fc35a9f0f7ea964e00f6be64dcee
Lup Yuen Lee <luppy@appkaki.com>
2026-03-30 12:40:06 +0800

    Read JSON File OK
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,12 @@ fn main() {
     // Generate the Merged Build JSON for each Run ID
     // Stop iterating when Timestamp is Older than 5 Days
     // Sort by Timestamp in Descending Order (Latest First)
-    fetch_job_pr();
+
+    // For each Run ID (Job ID), Fetch the Job-PR JSON
+    let job_pr = fetch_job_pr();
+    println!("job_pr:\n{job_pr:?}\n");
+
+    // TODO: Merge the Build JSON into the Job-PR JSON    
 
     // TODO: Generate the HTML Table from Merged Build JSON:
     // Write the HTML Table to a Static File
@@ -30,8 +35,8 @@ fn main() {
     println!("HTML Table:\n{html_table}");
 }
 
-// Fetch the Job-PR-Build JSON for a Given Run ID (Job ID)
-fn fetch_job_pr() {
+// Fetch the Job-PR JSON for a Given Run ID (Job ID)
+fn fetch_job_pr() -> Result<String, Box<dyn std::error::Error>> {
     // Given a Run ID (Job ID): Locate the Job-PR from the JSON File
     // let run_id = 23653869993;  // sim-02:sim:login: >>>> WARNING: YOU ARE USING DEFAULT PASSWORD KEYS (CONFIG_FSUTILS_PASSWD_KEY1-4)!!! PLEASE CHANGE IT!!! <<<< \n 17d16 \n < CONFIG_BOARD_ETC_ROMFS_PASSWD_PASSWORD=\"Administrator\" \n Saving the new configuration file
     // let run_id = 23669957941;  // Successful
@@ -39,7 +44,7 @@ fn fetch_job_pr() {
     // let run_id = 1234;  // Doesn't exist
 
     // Open the JSON File and create a Streaming JSON Reader
-    let file = std::fs::read(JOB_PR_JSON).unwrap();
+    let file = std::fs::read(JOB_PR_JSON)?;
     let json_reader = SimpleJsonReader::new(file.as_slice());
 
     // For each Job-PR record in the array...
@@ -67,23 +72,23 @@ fn fetch_job_pr() {
     // Quit if index not found
     if index.is_none() {
         println!("Run ID {run_id} not found in {JOB_PR_JSON}. Please regenerate the JSON File.");
-        return;
+        return Err("Run ID not found".into());
     }
     let index = index.unwrap() as u32;
 
     // Jump to the Found Index in the Job-PR array
-    let file = std::fs::read(JOB_PR_JSON).unwrap();
+    let file = std::fs::read(JOB_PR_JSON)?;
     let mut json_reader = JsonStreamReader::new(file.as_slice());
     let path = &json_path![index];
-    json_reader.seek_to(path).unwrap();
+    json_reader.seek_to(path)?;
 
     // Extract the Job-PR
     let mut writer = Vec::<u8>::new();
     let mut json_writer = JsonStreamWriter::new(&mut writer);
-    json_reader.transfer_to(&mut json_writer).unwrap();
-    json_writer.finish_document().unwrap() ;
-    let job_pr = String::from_utf8(writer).unwrap();
-    println!("job_pr:\n{job_pr}\n");
+    json_reader.transfer_to(&mut json_writer)?;
+    json_writer.finish_document()?;
+    let job_pr = String::from_utf8(writer)?;
 
-    // TODO: Merge the Build JSON into the Job-PR JSON    
+    println!("job_pr:\n{job_pr}\n");
+    Ok(job_pr)
 }
