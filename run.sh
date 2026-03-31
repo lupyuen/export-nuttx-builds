f40335f20ab94c076d5312ec866227b8036bfb13
Lup Yuen Lee <luppy@appkaki.com>
2026-03-31 17:42:41 +0800

    Dump 2 days of GitHub Data roughly every 8th time, else dump 1 day
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- run.sh
+++ run.sh
@@ -10,15 +10,23 @@ set -x  #  Echo commands
 ## Loop forever
 while true; do
 
-  ## Dump the GitHub PRs, Jobs and Job Durations for the past 2 days.
+  ## Dump 2 days of GitHub Data roughly every 8th time, else dump 1 day.
+  ## So we don't miss any GitHub Data during the UTC Midnight Transition across 2 days.
+  if [[ $(($RANDOM % 8)) == 0 ]]; then
+      num_days=2
+  else
+      num_days=1
+  fi
+
+  ## Dump the GitHub PRs, Jobs and Job Durations for the past 1 or 2 days.
   ## They will be populated in the pr, job and duration folders.
   ## Calls ../parse-nuttx-builds to download and parse the GitHub Build Logs.
   ## Which will populate the success, warning and error folders.
   pushd ../nuttx-github-jobs
   git pull
-  ./dump-github-jobs.sh 2
+  ./dump-github-jobs.sh $num_days
   git pull && git add .
-  git commit --all --message="Updated PRs, Jobs, Durations by \`dump-github-jobs.sh 2\`" && git push
+  git commit --all --message="Updated PRs, Jobs, Durations by \`dump-github-jobs.sh $num_days\`" && git push
   popd
 
   ## Join the PRs, Jobs and Durations into one TSV file and one JSON file:
